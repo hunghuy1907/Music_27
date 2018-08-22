92dcd880662a9f2e837103b10ac031e38f73c199
huyhung1907 <hunghuy1907@gmail.com>
2018-08-22 16:24:23 +0700

    init_MVP

---
+++ app/src/main/java/com/framgia/music_27/screen/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.framgia.music_27.screen.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by le.quang.dao on 10/03/2017.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+}
--- app/src/main/java/com/framgia/music_27/screen/home/HomeActivity.java
+++ app/src/main/java/com/framgia/music_27/screen/home/HomeActivity.java
@@ -1,13 +1,15 @@
-package com.framgia.music_27;
+package com.framgia.music_27.screen.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.framgia.music_27.R;
+
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
